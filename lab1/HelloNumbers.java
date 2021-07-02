d073c6ed7009f098a7d6652cc07180215103b1d1
Xiaohan <zhaoxhmail@163.com>
2021-07-02 17:58:07 +0800

    completed first part of lab1

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -1,8 +1,10 @@
 public class HelloNumbers {
   public static void main(String[] args) {
     int x = 0;
+    int total = 0;
     while (x < 10) {
-      System.out.print(x + " ");
+      total = total + x;
+      System.out.println(total + " ");
       x = x + 1;
     }
   }
